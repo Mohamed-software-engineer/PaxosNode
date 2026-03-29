58013c54d6f03bb620e6a3cd09d26704da0166fe
Mohamed-software-engineer <mohamedsaad01011955461@gmail.com>
2026-03-29 21:46:31 +0200

    new deployment for backend

--- frontend/src/config/nodes.js
+++ frontend/src/config/nodes.js
@@ -1,7 +1,7 @@
 export const NODES = [
-  { id: 1, name: "Node 1", baseUrl: "https://paxosnode1-production.up.railway.app" },
-  { id: 2, name: "Node 2", baseUrl: "https://paxosnode2-production.up.railway.app" },
-  { id: 3, name: "Node 3", baseUrl: "https://paxosnode3-production.up.railway.app" },
-  { id: 4, name: "Node 4", baseUrl: "https://paxosnode4-production.up.railway.app" },
-  { id: 5, name: "Node 5", baseUrl: "https://paxosnode5-production.up.railway.app" },
+  { id: 1, name: "Node 1", baseUrl: "https://thermal-bring-mtv-laboratory.trycloudflare.com" },
+  { id: 2, name: "Node 2", baseUrl: "https://certified-logos-liverpool-location.trycloudflare.com" },
+  { id: 3, name: "Node 3", baseUrl: "https://convertible-singh-modules-treasury.trycloudflare.com" },
+  { id: 4, name: "Node 4", baseUrl: "https://limits-suspected-assess-alto.trycloudflare.com" },
+  { id: 5, name: "Node 5", baseUrl: "https://express-entitled-infant-var.trycloudflare.com" },
 ];
