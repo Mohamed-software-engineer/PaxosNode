c229a59899c19c46bf5c5a9a1f2b2f13c97c7fa0
Mohamed-software-engineer <mohamedsaad01011955461@gmail.com>
2026-04-02 03:28:04 +0200

    update domains

--- frontend/src/config/nodes.js
+++ frontend/src/config/nodes.js
@@ -1,7 +1,7 @@
 export const NODES = [
-  { id: 1, name: "Node 1", baseUrl: "https://lunch-add-plants-sponsorship.trycloudflare.com" },
-  { id: 2, name: "Node 2", baseUrl: "https://packets-hollow-eval-mystery.trycloudflare.com " },
-  { id: 3, name: "Node 3", baseUrl: "https://upgrade-attorney-poem-wrist.trycloudflare.com" },
-  { id: 4, name: "Node 4", baseUrl: "https://nationally-cottage-industry-providence.trycloudflare.com" },
-  { id: 5, name: "Node 5", baseUrl: "https://ensure-pgp-sean-actions.trycloudflare.com" },
+  { id: 1, name: "Node 1", baseUrl: "https://around-festival-score-reynolds.trycloudflare.com" },
+  { id: 2, name: "Node 2", baseUrl: "https://observation-remain-varies-steam.trycloudflare.com" },
+  { id: 3, name: "Node 3", baseUrl: "https://began-image-recorder-watson.trycloudflare.com" },
+  { id: 4, name: "Node 4", baseUrl: "https://journalist-disable-thinking-explaining.trycloudflare.com" },
+  { id: 5, name: "Node 5", baseUrl: "https://places-mere-offices-maps.trycloudflare.com" },
 ];
